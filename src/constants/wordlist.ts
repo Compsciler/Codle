42be779c431d8a20465d16a200945c8b4782ef5a
Roger Wang <rogerwang2520@gmail.com>
2022-05-14 10:59:20 -0700

    Prepare app for Heroku

--- src/constants/wordlist.ts
+++ src/constants/wordlist.ts
@@ -1,3 +1,4 @@
 export const WORDS = [
   ';A++)',
+  '->A!=',
 ]
--- src/services/scores.js
+++ src/services/scores.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/api/scores'
+const baseUrl = 'https://codle.herokuapp.com/api/scores'
 
 /*
 const getAll = () => {
